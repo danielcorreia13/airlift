378935d63f55ca3923a0e642218841839d6f752c
Daniel Lopes <daniel.vnl@ua.pt>
2021-04-17 23:40:17 +0100

    Commit para testar

--- src/Main/AirLift.java
+++ src/Main/AirLift.java
@@ -1,6 +1,28 @@
 package Main;
 
+import ActiveEntity.Hostess;
+import ActiveEntity.Passenger;
+import ActiveEntity.Pilot;
+
+import DepartureAirport.DepartureAirport;
+import DepartureAirport.IDepartureAirport_Hostess;
+import DepartureAirport.IDepartureAirport_Passenger;
+import DepartureAirport.IDepartureAirport_Pilot;
+
+import DestinationAirport.DestinationAirport;
+import DestinationAirport.IDestinationAirport_Passenger;
+import DestinationAirport.IDestinationAirport_Pilot;
+
+import Plane.IPlane_Hostess;
+import Plane.IPlane_Passenger;
+import Plane.IPlane_Pilot;
+import Plane.Plane;
+
 public class AirLift
 {
-
+	
+	public static void main(String args[])
+	{
+		System.out.println("Teste");
+	}
 }
